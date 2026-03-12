4a5f1f7be71b35f22a84e3781e77832e7a5a37e1
Md. Akash Ahmed <mdakash30453@gmail.com>
2026-03-12 23:51:58 +0600

    frist try

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/home.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarThemeData(
-          centerTitle: true,
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.blue
-        ),
-        scaffoldBackgroundColor: Colors.blue.shade100
-      ),
-      title: 'WhatsApp Clone',
-      home: Home(),
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            appBarTheme: AppBarThemeData(
+              centerTitle: true,
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.blue,
+            ),
+            scaffoldBackgroundColor: Colors.blue.shade100,
+          ),
+          title: 'WhatsApp Clone',
+          home: Home(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,44 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Tab Bar'),
-          bottom: TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.home),
-              text: 'Home',
-            ),
-            Tab(
-              icon: Icon(Icons.favorite),
-              text: 'Fav',
-            ),
-            Tab(
-              icon: Icon(Icons.settings),
-              text: 'Settings',
-            )
-          ]),
-        ),
-      body: TabBarView(children: [
-        Column(
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-              
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  
+                  Text(
+                    'Hello, Md Akash',
+                    style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 13),
+        
+                  CircleAvatar(
+                    radius: 20.r,
+                    backgroundImage: NetworkImage(
+                      'https://media.licdn.com/dms/image/v2/D4D03AQFvwSL9TeVHVA/profile-displayphoto-scale_200_200/B4DZiiOC4GGgAc-/0/1755068250704?e=2147483647&v=beta&t=CwZzl7bG0j-wFxX0m4C4TpT4enC29-UIIlLFA5zZEuk',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+           
           ],
         ),
-        Column(
-        children: [Container(height: 300, color: Colors.green)],
-            ),
-
-            Column(
-              children: [Container(height: 300, color: Colors.black)],
-            ),
-      ]),
       ),
     );
   }
